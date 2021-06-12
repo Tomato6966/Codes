5639604ca7b290aa77e23a5af0f64f720bf9d88e
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-06-12 14:55:30 +0200

    Create Make_Your_Bot_React_FASTER.js

---
+++ Make_Your_Bot_React_FASTER.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+//creating the client
+const client = new Discord.Client({
+    disableMentions: "all",
+    shards: "auto",
+    restTimeOffset: 0
+});
+//Defining all emojis
+const emojis = [ "🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🍈"]
+//logging when bot is ready
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`) );
+//executing the Cmd
+client.on('message', msg => {
+  if(msg.author.id != "442355791412854784") return
+  if (msg.content === '!react') {
+    const date1 = Date.now();
+    msg.reply('REACTING...').then(async msg => {
+        for(const emoji of emojis)
+            await msg.react(emoji)
+        msg.edit(`It took me: ${(Date.now() - date1) / 1000} Seconds to react with ${emojis.length} Emojis!`)
+    })
+  }
+});
+//logging in the BOT
+client.login("YOUR BOT TOKEN")
