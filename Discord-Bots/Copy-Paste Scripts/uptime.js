cc8e9a040d867ef4d14e0cf39167e6ea8726f91e
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-07-05 11:08:20 +0200

    Add files via upload

---
+++ Discord-Bots/Copy-Paste Scripts/uptime.js
@@ -0,0 +1,47 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+var ee = require("../../botconfig/embed.json");
+const emoji = require(`../../botconfig/emojis.json`);
+const {
+  duration
+} = require("../../handlers/functions")
+const moment = require("moment")
+module.exports = {
+  name: "uptime",
+  category: "🔰 Info",
+  aliases: [""],
+  usage: "uptime",
+  description: "Returns the duration on how long the Bot is online",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    let es = client.settings.get(message.guild.id, "embed")
+    try {
+      let date = new Date()
+      let timestamp = date.getTime() - Math.floor(client.uptime);
+      message.channel.send(new MessageEmbed()
+        .setColor(es.color).setThumbnail(es.thumb ? es.footericon : null)
+        .setFooter(es.footertext, es.footericon)
+        .setTitle(`:white_check_mark: **${client.user.username}** Uptime`)
+        .setDescription(`\`\`\`css\n${duration(client.uptime).map(i=> `${i}`).join(", ")}\`\`\``)
+        .addField("**Date Launched**",  moment(timestamp).format("LLLL"))
+      );
+    } catch (e) {
+      console.log(String(e.stack).bgRed)
+      return message.channel.send(new MessageEmbed()
+        .setColor(es.wrongcolor).setFooter(es.footertext, es.footericon)
+        .setTitle(`<:no:833101993668771842> An error occurred`)
+        .setDescription(`\`\`\`${String(JSON.stringify(e)).substr(0, 2000)}\`\`\``)
+      );
+    }
+  }
+}
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
