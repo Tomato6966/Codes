71ed977a37a49a8a383860b4986322587dd5e380
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-20 08:21:43 +0200

    Update autoStageChannelSpeak.js

--- Discord-Bots-v13/Copy-Paste Scripts/autoStageChannelSpeak.js
+++ Discord-Bots-v13/Copy-Paste Scripts/autoStageChannelSpeak.js
@@ -1,39 +1,9 @@
-client.on("voiceStateUpdate", (oldState, newState) => {
-    if (
-        (!oldState.streaming && newState.streaming) ||
-        (oldState.streaming && !newState.streaming) ||
-        (!oldState.serverDeaf && newState.serverDeaf) ||
-        (oldState.serverDeaf && !newState.serverDeaf) ||
-        (!oldState.serverMute && newState.serverMute) ||
-        (oldState.serverMute && !newState.serverMute) ||
-        (!oldState.selfDeaf && newState.selfDeaf) ||
-        (oldState.selfDeaf && !newState.selfDeaf) ||
-        (!oldState.selfMute && newState.selfMute) ||
-        (oldState.selfMute && !newState.selfMute) ||
-        (!oldState.selfVideo && newState.selfVideo) ||
-        (oldState.selfVideo && !newState.selfVideo)
-    ) return;
-    if (!oldState.channelID && newState.channelID) {
-        if(newState.channel.type == "GUILD_STAGE_VOICE" && newState.guild.me.voice.suppress){
-            try{
-                await newState.guild.me.voice.setSuppressed(false)
-            }catch{
-
-            }
+client.on("voiceStateUpdate", async (oldState, newState) => {
+    if (newState.channelId && newState.channel.type == "GUILD_STAGE_VOICE" && newState.guild.me.voice.suppress) {
+        try {
+            await newState.guild.me.voice.setSuppressed(false);
+        } catch (e) {
+            console.log(e)
         }
-        return;
     }
-    if (oldState.channelID && !newState.channelID) {
-        return;
-    }
-    if (oldState.channelID && newState.channelID) {
-        if(newState.channel.type == "GUILD_STAGE_VOICE" && newState.guild.me.voice.suppress){
-            try{
-                await newState.guild.me.voice.setSuppressed(false)
-            }catch{
-
-            }
-        }
-        return;
-    }
-});
+})
